cd76fd054a6e826f8800f206aaeeacd2a7c71c57
plaibo1 <49759228+plaibo1@users.noreply.github.com>
2019-11-07 19:45:17 +0300

    sound paths

--- chatStory/app/js/story-1.js
+++ chatStory/app/js/story-1.js
@@ -4,11 +4,11 @@ let person_3 = 'Jopnik';
 
 let story1 = [
 	{name: person_2, message: 'Hello who are u?'},
-	{name: person_1, message: 'Hello im mark jacobs, do u remember me?', img: '../img/misha.jpg'},
+	{name: person_1, message: 'Hello im mark jacobs, do u remember me?', img: 'img/misha.jpg'},
 	
 	{ name: person_2, message: 'Hi, sure'},
-	{ name: person_1, message: 'How are you?', img: '../img/misha.jpg'},
-	{ name: person_3, message: 'Привет я вас ебал это я', img: '../img/icons/frankenstein.svg'},
+	{ name: person_1, message: 'How are you?', img: 'img/misha.jpg'},
+	{ name: person_3, message: 'Привет я вас ебал это я', img: 'img/icons/frankenstein.svg'},
 
 
 ];
